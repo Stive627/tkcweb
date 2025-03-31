b24e1ad5f1ac95eadfe2dcd73d81dcaa2b0faac6
Stive627 <fossistive627@gmail.com>
2025-03-31 20:12:50 +0530

    i disabled the button

--- Components/Register/RegistrationForm.js
+++ Components/Register/RegistrationForm.js
@@ -16,7 +16,7 @@ function RegistrationForm({email, password, repassword, setEmail, setPassword, s
           <ErrorOutlineIcon sx={{fontSize:18}}/>
           <p className=' text-[9px]'>The password should contain at least 06 characters, and 1 uppercase character.</p>
         </div>
-        <div className = 'flex justify-center'><button type='submit'  className={`text-white font-semibold w-full p-2 rounded-md cursor-pointer`} style={{backgroundColor:validFom?'rgba(7, 60, 160, 1)':'rgba(101, 137, 204, 1)'}} disabled={!validFom}>Register</button></div>
+        <div className = 'flex justify-center'><button type='submit'  className={`text-white font-semibold w-full p-2 rounded-md cursor-pointer`} style={{backgroundColor:validFom?'rgba(7, 60, 160, 1)':'rgba(101, 137, 204, 1)', cursor:!validFom?'not-allowed':'pointer'}} disabled={!validFom}>Register</button></div>
       </form>
         <button style={{borderColor:'rgba(0, 0, 0, 0.27)'}} className=' border rounded-md w-full cursor-pointer my-3 flex justify-center py-2'> <div className=' flex flex-row gap-4'><Image width={25} height={25} src={fetchLink('logogoogle.png')} alt='logo google'/>{'  '} <p className=' text-[16px]'>Continue with Google</p></div></button>
         <p className=' text-center text-[16px]'>Already have an account? <Link className=' underline text-blue-600' href={'/login'}>Login here</Link> </p>
