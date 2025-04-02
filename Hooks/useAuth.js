0649e19ca47b220d1fbf6a8c3b4827b1fe7cc66d
Stive627 <fossistive627@gmail.com>
2025-04-02 17:13:41 +0530

    sixth commit

--- Hooks/useAuth.js
+++ Hooks/useAuth.js
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 export default function useAuth(){
     const [auth, setAuth] = useState(undefined)
     useEffect(() => {
-        axios({url:fetchLink('user/connect'), method:'GET', headers:{"Content-Type":"application/json"}})
+        axios({url:fetchLink('user/connect'), method:'GET', withCredentials:true, headers:{"Content-Type":"application/json",}})
         .then((value)=> {setAuth(value.data.authenticated); console.log(value.data)})
         .catch(err => console.error(err))
     },[])
