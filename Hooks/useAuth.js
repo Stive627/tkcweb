112db44688be853e051773dbc9a05372c47a89e6
Stive627 <fossistive627@gmail.com>
2025-04-02 15:33:36 +0530

    first commit

---
+++ Hooks/useAuth.js
@@ -0,0 +1,13 @@
+import fetchLink from "@/Functions/fetchLink";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function useAuth(){
+    const [auth, setAuth] = useState(undefined)
+    useEffect(() => {
+        axios({url:fetchLink('user/connect'), method:'GET', headers:{"Content-Type":"application/json"}})
+        .then((value)=> {setAuth(value.data.authenticated); console.log(value.data)})
+        .catch(err => console.error(err))
+    },[])
+    return auth
+}
--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
 import HOmeTKC from "@/Components/Home/HomeTKC";
+import Login from "@/Components/Login/Login";
+import useAuth from "@/Hooks/useAuth";
 
 export default function Home() {
-  return (
-    <HOmeTKC/>
-  );
+  const auth = useAuth()
+  switch(auth){
+    case undefined:
+      return <div></div>
+    case false:
+      return <Login/>
+    case true:
+      return <HOmeTKC/>
+  }
 }
