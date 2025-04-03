1bac17eaa843efc4169c9de1e43d71bf15d0c706
Stive627 <fossistive627@gmail.com>
2025-04-03 08:52:14 +0530

    i am going to school

--- Hooks/useAuth.js
+++ Hooks/useAuth.js
@@ -5,8 +5,8 @@ import { useEffect, useState } from "react";
 
 export default function useAuth(){
     const [auth, setAuth] = useState(undefined)
-    const token = localStorage.getItem('tkc_token')
     useEffect(() => {
+        const token = localStorage.getItem('tkc_token')
         axios({url:fetchLink('user/connect'), method:'GET', headers:{"Content-Type":"application/json", Authorization:`Bearer ${token}`}})
         .then((value)=> {setAuth(value.data.authenticated); console.log(value.data)})
         .catch(err => console.error(err))
