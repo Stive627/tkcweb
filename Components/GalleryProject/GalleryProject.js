08c0aafea216120410665228c1965bb7f715abb6
Stive627 <fossistive627@gmail.com>
2025-04-08 01:14:55 +0530

    bye bye

--- Components/GalleryProject/GalleryProject.js
+++ Components/GalleryProject/GalleryProject.js
@@ -48,11 +48,11 @@ function GalleryProject({setShowGallery, images, clickIndx}) {
     <div onClick={()=> setShowGallery(false)} style={{backgroundColor:'rgba(0, 0, 0, 0.23)'}} className=' absolute top-0 left-0 right-0 bottom-0 w-full h-full '>
     <div className=' w-full h-full flex justify-center items-center'>
         <div onClick={(e)=> e.stopPropagation()} className={`${large? 'w-1/2 h-96 ':'w-full mx-2 h-80'}  border bg-white flex flex-row items-center p-2 justify-between gap-3`}>
-        {imglength > 0 ?<button onClick={()=>handleClickBack()} className={` w-10 h-10 flex justify-center items-center rounded-full ${currIndx >0 && 'bg-black ' }`}><ArrowBackIosIcon className=' relative left-1' sx={{color:'white'}}/></button>:<p></p>}
+        {imglength !==1 ?<button onClick={()=>handleClickBack()} className={` w-10 h-10 flex justify-center items-center rounded-full ${currIndx >0 && 'bg-black ' }`}><ArrowBackIosIcon className=' relative left-1' sx={{color:'white'}}/></button>:<p></p>}
         <div className={`h-full ${large? 'w-96':'w-65'} border flex flex-row overflow-hidden`}>
             {images?.map((elt, indx) => <Image ref={(node) => {imgRef.current.set(elt, node); return()=>imgRef.current.delete(elt)}} key={indx} src={fetchLink(elt.slice(7))} width={large? 385 : 265} height={400} alt={`image no${indx}`}/>)}
         </div>
-        {imglength > 0 ?<button disabled={currIndx >= imglength -1} onClick={()=> handleClick()} style={{backgroundColor:currIndx === imglength -1 ? 'rgba(217, 217, 217, 1)':'black'}} className=' w-10 h-10 flex justify-center items-center rounded-full'><ArrowForwardIosIcon className=' relative left-0.5' sx={{color:'white'}}/></button>:<p></p>}
+        {imglength !==1 ?<button disabled={currIndx >= imglength -1} onClick={()=> handleClick()} style={{backgroundColor:currIndx === imglength -1 ? 'rgba(217, 217, 217, 1)':'black'}} className=' w-10 h-10 flex justify-center items-center rounded-full'><ArrowForwardIosIcon className=' relative left-0.5' sx={{color:'white'}}/></button>:<p></p>}
         </div>
     </div>
     </div>
--- Components/SnippetsTips/SingleContentLine.js
+++ Components/SnippetsTips/SingleContentLine.js
@@ -3,9 +3,11 @@ import React, { useState } from 'react'
 import './snippet.css'
 import fetchLink from '@/Functions/fetchLink'
 import CloseIcon from '@mui/icons-material/Close';
+import GalleryProject from '../GalleryProject/GalleryProject';
 
 function SingleContentLine({indx, handlOpen, openTip, elt, setOpenTip, handleDeleteTips}) {
   const [show, setShow] = useState('false')
+  const [showGallery, setShowGallery] = useState(false)
   return (
     <div onClick={()=>{if(show){setOpenTip(''); setShow('')}else{handlOpen(indx); setShow(String(indx))}}} style={{backgroundColor:indx%2 === 1 ? 'rgba(0, 0, 0, 0.04)':'white'}} className='cursor-pointer pb-2'>
       <div className=' w-full flex flex-row justify-between  items-center gap-3.5'>
@@ -21,8 +23,9 @@ function SingleContentLine({indx, handlOpen, openTip, elt, setOpenTip, handleDel
           <div className={`border border-gray-50 `}>
               <p>{elt.description}</p>
           </div>
-          {elt.image !== 'undefined' && elt.image && <div className=' flex justify-center'><Image src={typeof elt.image === 'string' ? fetchLink(elt.image.slice(6)) : URL.createObjectURL(elt.image[0])} width={200} height={200} alt={`Snippet image no${indx}`}/></div>} 
+          {elt.image !== 'undefined' && elt.image && <div className=' flex justify-center'><Image onClick={(e)=> { e.stopPropagation(); setShowGallery(true)}} src={typeof elt.image === 'string' ? fetchLink(elt.image.slice(6)) : URL.createObjectURL(elt.image[0])} width={200} height={200} alt={`Snippet image no${indx}`}/></div>} 
       </div>
+      {showGallery && <GalleryProject setShowGallery={setShowGallery} clickIndx={0} images={[elt.image]}/>}
     </div>
   )
 }
