578ea340cf8382fc5ace458afb9091cb30dd5ad1
Stive627 <fossistive627@gmail.com>
2025-04-08 00:56:45 +0530

    last commit of today

---
+++ Components/GalleryProject/GalleryProject.js
@@ -0,0 +1,62 @@
+import fetchLink from '@/Functions/fetchLink';
+import { useScreen } from '@/Hooks/useScreen'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import Image from 'next/image';
+import React, { useEffect, useRef, useState } from 'react'
+
+function GalleryProject({setShowGallery, images, clickIndx}) {
+    const imgRef = useRef(new Map())
+    const [currIndx, setCurrIndx] = useState(0)
+    const imglength = images.length
+    console.log(images)
+    const large = useScreen()
+    useEffect(()=>{
+        const node = imgRef.current.get(images[clickIndx])
+        node.scrollIntoView({
+            behaviour:'smooth',
+            block:'nearest',
+            inline:'center'
+        })
+    }, [])
+    const handleClick = () => {
+        const node = imgRef.current.get(images[currIndx + 1])
+        node.scrollIntoView({
+            behaviour:'smooth',
+            block:'nearest',
+            inline:'center'
+        })
+        setCurrIndx((currIndx + 1)%images.length)
+    }
+
+
+    const handleClickBack = () => {
+        if(currIndx>0){
+            const node = imgRef.current.get(images[currIndx - 1])
+            node.scrollIntoView({
+                behaviour:'smooth',
+                block:'nearest',
+                inline:'center'
+            })
+            setCurrIndx(currIndx -1)
+        }
+        else{
+            return;
+        }
+    }
+    return (
+    <div onClick={()=> setShowGallery(false)} style={{backgroundColor:'rgba(0, 0, 0, 0.23)'}} className=' absolute top-0 left-0 right-0 bottom-0 w-full h-full '>
+    <div className=' w-full h-full flex justify-center items-center'>
+        <div onClick={(e)=> e.stopPropagation()} className={`${large? 'w-1/2 h-96 ':'w-full mx-2 h-80'}  border bg-white flex flex-row items-center p-2 justify-between gap-3`}>
+        {imglength > 0 ?<button onClick={()=>handleClickBack()} className={` w-10 h-10 flex justify-center items-center rounded-full ${currIndx >0 && 'bg-black ' }`}><ArrowBackIosIcon className=' relative left-1' sx={{color:'white'}}/></button>:<p></p>}
+        <div className={`h-full ${large? 'w-96':'w-65'} border flex flex-row overflow-hidden`}>
+            {images?.map((elt, indx) => <Image ref={(node) => {imgRef.current.set(elt, node); return()=>imgRef.current.delete(elt)}} key={indx} src={fetchLink(elt.slice(7))} width={large? 385 : 265} height={400} alt={`image no${indx}`}/>)}
+        </div>
+        {imglength > 0 ?<button disabled={currIndx >= imglength -1} onClick={()=> handleClick()} style={{backgroundColor:currIndx === imglength -1 ? 'rgba(217, 217, 217, 1)':'black'}} className=' w-10 h-10 flex justify-center items-center rounded-full'><ArrowForwardIosIcon className=' relative left-0.5' sx={{color:'white'}}/></button>:<p></p>}
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default GalleryProject
--- Components/Projects/ContentProject.js
+++ Components/Projects/ContentProject.js
@@ -1,18 +1,20 @@
 import fetchLink from '@/Functions/fetchLink'
 import getImgName from '@/Functions/getImgName'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import './project.css'
 import { useScreen } from '@/Hooks/useScreen'
+import GalleryProject from '../GalleryProject/GalleryProject'
 
 function ContentProject({project}) {
+  const [showGallery, setShowGallery] = useState({status:false, imgIndx:''})
   const large = useScreen()
   return (
     <div className='w-full'>
       <div className=' w-full flex justify-center mb-5'>
         <div className={`flex justify-between items-center ${large?'w-2/3':'w-full'}`}>
           <p className=' text-[21px] font-bold'>{project.title}</p>
-          <div style={{borderColor:'rgba(0, 0, 0, 0.3)'}} className=' px-7 py-1 rounded-sm flex justify-center items-center border'>
+          <div style={{borderColor:'rgba(0, 0, 0, 0.3)'}} className={`px-7 py-1 rounded-sm flex justify-center items-center border ${large && 'relative right-5'}`}>
             <p style={{color:'rgba(0, 0, 0, 0.77)'}}>{project.department}</p>
           </div>
         </div>
@@ -24,11 +26,12 @@ function ContentProject({project}) {
             project.images.map((elt, indx) =>(
               <div key={indx}>
                 <p className=' text-[16px] mb-1'>{getImgName(elt)}</p>
-                <Image src={fetchLink(elt.slice(6))} width={500} height={500} alt={`image of project ${elt.title} no${indx+1}`}/>
+                <Image onClick={()=> setShowGallery({status:true, imgIndx:indx})} src={fetchLink(elt.slice(6))} width={500} height={500} alt={`image of project ${elt.title} no${indx+1}`}/>
               </div>
             ))
           }
         </div>
+        {showGallery.status && <GalleryProject setShowGallery={setShowGallery} clickIndx={showGallery.imgIndx} images={project.images}/>}
         </div>
     </div>
   )
